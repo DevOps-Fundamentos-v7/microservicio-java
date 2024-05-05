d1a5c54e94b941fe5f866b90db5db538876a4661
altheus26 <131408386+altheus26@users.noreply.github.com>
2024-05-05 17:18:12 -0400

    Variable bloqueante

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		String password = '12345';
+		String password = "12345";
 		return "Hola, soy Esteban Espinoza";
 	}
 }
